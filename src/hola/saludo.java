19198a3e0580fa2f2ca60e85059be2110819f9aa
Diego Lopez <awsmr1607@gmail.com>
2018-05-04 11:36:47 -0700

    v1

---
+++ src/hola/saludo.java
@@ -0,0 +1,11 @@
+package hola;
+
+public class saludo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Buenas tardes a todoos");
+
+	}
+
+}
