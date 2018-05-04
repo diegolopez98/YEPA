95a9f87aa69a323d82b69bc47abba4d18ed71482
Diego Lopez <awsmr1607@gmail.com>
2018-05-04 11:38:29 -0700

    v2

--- src/hola/saludo.java
+++ src/hola/saludo.java
@@ -5,6 +5,7 @@ public class saludo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Buenas tardes a todoos");
+		System.out.println("Hola me llamo CArlos y soy un maco al csgo");
 
 	}
 
